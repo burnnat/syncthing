6b1a8c6fd9005b4a6f2e462664caacd67b126027
Nat Burns <nbaccount@burnskids.com>
2015-01-26 21:29:32 -0500

    Lower minimum TLS version to 1.1.
    The Forge library does not yet support TLS 1.2, so relaxing this
    requirement allows interoperability for Syncthing on Chrome OS without
    waiting for a (yet unknown) Forge release with TLS 1.2 implemented.

--- cmd/syncthing/main.go
+++ cmd/syncthing/main.go
@@ -462,7 +462,7 @@ func syncthingMain() {
 		ClientAuth:             tls.RequestClientCert,
 		SessionTicketsDisabled: true,
 		InsecureSkipVerify:     true,
-		MinVersion:             tls.VersionTLS12,
+		MinVersion:             tls.VersionTLS11,
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
